f77c77f5e9d302b88e2d743162dd51dbf05052b2
NicoGuerrero11 <nicoguerrero0800@gmail.com>
2024-05-16 19:27:42 -0600

    creamos el dia01

---
+++ day_01/variable.js
@@ -0,0 +1,38 @@
+// Los comentarios pueden hacel que el codigo sea legible
+// Bienvenido a 30DaysOfJavaScript
+
+/* Los comentarios pueden hacer el codigo legibl, facil de usar e informativo */
+
+let palabra = "string";
+let verdadero = true;
+let nada = null;
+let indefinido = undefined;
+
+console.log(typeof palabra);
+console.log(typeof verdadero);
+console.log(typeof nada);
+console.log(typeof indefinido);
+
+let valor1;
+let valor2;
+let valor3;
+let valor4;
+
+let valor5 = "valor 5";
+let valor6 = "valor 6";
+let valor7 = "valor 7";
+let valor8 = "valor 8";
+
+
+let nombre;
+let apellido;
+let estadoCivil;
+let pais;
+let edad;
+
+let name, lastName, civil, born, age;
+
+let myAge = 25;
+let yourAge = 30;
+console.log("I am",myAge, "years old.");
+console.log("You are", yourAge,"years old")
